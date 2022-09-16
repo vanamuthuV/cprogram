97b743222e2bed452e02a76a91ca559fa885d602
VANAMUTHU V <109649687+vanamuthuV@users.noreply.github.com>
2022-09-16 07:48:00 +0530

    Add files via upload

---
+++ display n natural no.c
@@ -0,0 +1,13 @@
+// To display the N natural numbers
+
+  #include<stdio.h>
+    void main()
+       {
+        int n;
+        printf("enter the value of n :\n");
+        scanf("%d",&n);
+        for(int i=1;i<=n;i++)
+           {
+            printf("%d\n",i);
+           }
+       }
