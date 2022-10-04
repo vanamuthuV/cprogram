4c4eac527c3ea5270126a47edd1e941db2acf82f
VANAMUTHU V <109649687+vanamuthuV@users.noreply.github.com>
2022-10-04 09:45:26 +0530

    Add files via upload

---
+++ leap.c
@@ -0,0 +1,17 @@
+// To Find The Year Is Leap Year Or Not
+
+  #include<stdio.h>
+    void main()
+     {
+       int n,x;
+       printf("Enter a Year\n");
+       scanf("%d",&n);
+       if(n%4==0)
+         {
+          printf("The Entered Year Is a Leap Year\n");
+         }
+       else
+         {
+          printf("The Entered Year Not Is a Leap Year\n");
+         }
+     }
